e2b23e7a0dc597994928a1a03fdf53ea4b7f7093
Fabian Loewe <hyronx@outlook.com>
2020-05-26 16:23:07 +0200

    Updated footer and sidebars.json

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,17 +37,17 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('introduction.html', this.props.language)}>
+              Introduction
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+            {/*<a href={this.docUrl('doc2.html', this.props.language)}>
+              Guides
             </a>
             <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
-            </a>
+              API Reference
+            </a>*/}
           </div>
-          <div>
+          {/*<div>
             <h5>Community</h5>
             <a href={this.pageUrl('users.html', this.props.language)}>
               User Showcase
@@ -65,7 +65,7 @@ class Footer extends React.Component {
               rel="noreferrer noopener">
               Twitter
             </a>
-          </div>
+          </div>*/}
           <div>
             <h5>More</h5>
             <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
@@ -74,7 +74,7 @@ class Footer extends React.Component {
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
+              data-count-href="/AnimaNetwork/anima/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub">
@@ -105,7 +105,7 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
+        {/*<a
           href="https://opensource.facebook.com/"
           target="_blank"
           rel="noreferrer noopener"
@@ -116,7 +116,7 @@ class Footer extends React.Component {
             width="170"
             height="45"
           />
-        </a>
+        </a>*/}
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
